7e5bfef76ac48c3954d1d1fccb1de85f73f80238
Philippe <philippe-git@protonmail.com>
2024-10-23 11:37:21 +0200

    Update help command text

--- utils/Telegraf.js
+++ utils/Telegraf.js
@@ -14,14 +14,15 @@ const TELEGRAM_MESSAGE_OPTIONS = {
 
 const HELP_TEXT = `@CurveLendMonitorBot allows you to view loan information from [Curve Lend](https://lend.curve.fi/) and [crvUSD](https://crvusd.curve.fi/) for any address you want; it’ll also notify you of important changes to these loans’ health statuses\\.
 
-Here are the available commands:
+Available commands:
 
 \\- \`/add 0xADDRESS\`: Start monitoring an address; the bot will notify you of important changes to health statuses of this address’ loans on Curve Lend and crvUSD
 \\- \`/remove 0xADDRESS\`: Stop monitoring an address
 \\- /list: List all addresses you’ve added
 \\- /view: View loan information for all addresses you’ve added
 \\- /help: List all available commands
-`;
+
+[More information on GitHub](https://github.com/curvefi/curve-lend-telegram-bot?tab=readme-ov-file#curve-lend-telegram-bot)`;
 
 export {
   bot,
