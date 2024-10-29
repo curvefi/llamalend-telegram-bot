1e5c2fbcd22a36371b5fd4078e8f79a3cde9cf77
Philippe <philippe-git@protonmail.com>
2024-10-29 15:36:30 +0100

    Add current price and percent away from liq range

--- data/getUserOnchainData.js
+++ data/getUserOnchainData.js
@@ -1,3 +1,4 @@
+import BN from 'bignumber.js';
 import getAllCurveLendingVaults from '../utils/data/curve-lending-vaults.js';
 import { arrayToHashmap, flattenArray, removeNulls } from '../utils/Array.js';
 import LENDING_CONTROLLER_ABI from '../abis/LendingController.json' assert { type: 'json' };
@@ -67,6 +68,12 @@ const getUserLendingDataForNetwork = async (userAddresses, network) => {
           methodName: 'active_band',
           metaData: { userAddress, lendingVaultAddress, type: 'currentAmmBand' },
           network,
+        }, {
+          address: ammAddress,
+          abi: LENDING_AMM_ABI,
+          methodName: 'price_oracle',
+          metaData: { userAddress, lendingVaultAddress, type: 'priceOracle' },
+          network,
         }])
       );
     })
@@ -88,14 +95,15 @@ const getUserLendingDataForNetwork = async (userAddresses, network) => {
                   dataType === 'health' ? uintToBN(data, 18).dp(6) :
                     dataType === 'bandRange' ? data.map((n) => Number(n)).sort() : // Always asc order
                       dataType === 'currentAmmBand' ? Number(data) :
-                        dataType === 'priceRange' ? data.map((price, i) => uintToBN(price, vaultData.assets.collateral.decimals).dp(6)) :
-                          dataType === 'userState' ? {
-                            atRiskCollat: uintToBN(data[0], vaultData.assets.collateral.decimals).dp(6),
-                            atRiskBorrowed: uintToBN(data[1], vaultData.assets.borrowed.decimals).dp(6),
-                            debt: uintToBN(data[2], vaultData.assets.borrowed.decimals).dp(6),
-                            bandCount: Number(data[3]),
-                          } :
-                            data
+                        dataType === 'priceRange' ? data.map((price, i) => uintToBN(price, 18).dp(6)) :
+                          dataType === 'priceOracle' ? uintToBN(data, 18).dp(6) :
+                            dataType === 'userState' ? {
+                              atRiskCollat: uintToBN(data[0], vaultData.assets.collateral.decimals).dp(6),
+                              atRiskBorrowed: uintToBN(data[1], vaultData.assets.borrowed.decimals).dp(6),
+                              debt: uintToBN(data[2], vaultData.assets.borrowed.decimals).dp(6),
+                              bandCount: Number(data[3]),
+                            } :
+                              data
                 ),
               ];
             })
@@ -112,12 +120,20 @@ const getUserLendingDataForNetwork = async (userAddresses, network) => {
               positionData.currentAmmBand <= positionData.bandRange[1]
             );
 
+            const priceOracleDistFromRange = (
+              (!isInSoftLiq && !isInHardLiq) ? (
+                positionData.priceOracle.gte(BN.max(...positionData.priceRange)) ?
+                  (positionData.priceOracle.minus(BN.max(...positionData.priceRange)).div(positionData.priceOracle)).times(100) :
+                  undefined
+              ) : undefined
+            );
+
             const textLines = removeNulls([
               `State: *${isInHardLiq ? 'Hard Liquidation ⚠️' : isInSoftLiq ? 'Soft Liquidation ℹ️' : 'Healthy ✅'}*`,
               (isInHardLiq || isInSoftLiq) ? `Health: *${escapeNumberForTg(positionData.health.times(100).dp(4))}%*` : null,
               (isInHardLiq || isInSoftLiq) ? `Currently at risk: *${escapeNumberForTg(positionData.userState.atRiskCollat.dp(4))} ${vaultData.assets.collateral.symbol} and ${escapeNumberForTg(positionData.userState.atRiskBorrowed.dp(4))} ${vaultData.assets.borrowed.symbol}*` : null,
-              `Your collateral’s band range: *${escapeNumberForTg(positionData.bandRange[0])}→${escapeNumberForTg(positionData.bandRange[1])}* _\\(approx\\. corresponding liquidation price range: *${escapeNumberForTg(positionData.priceRange[0].dp(2))}→${escapeNumberForTg(positionData.priceRange[1].dp(2))}*\\)_`,
-              `Current AMM band: *${escapeNumberForTg(positionData.currentAmmBand)}*`,
+              `Liquidation band range: ${escapeNumberForTg(positionData.bandRange[0])}→${escapeNumberForTg(positionData.bandRange[1])} \\(current AMM band: ${escapeNumberForTg(positionData.currentAmmBand)}\\)`,
+              `Approx\\. liquidation price range:* ${escapeNumberForTg(positionData.priceRange[0].dp(2))}→${escapeNumberForTg(positionData.priceRange[1].dp(2))} *\\(current price: *${escapeNumberForTg(positionData.priceOracle.dp(2))}*${priceOracleDistFromRange !== undefined ? ` ≈ ${escapeNumberForTg(priceOracleDistFromRange.dp(2))}\\% away` : ''}\\)`,
             ]);
 
             const textPositionRepresentation = `\n  \\- Borrowing *${vaultData.assets.borrowed.symbol}* against *${vaultData.assets.collateral.symbol}* \\(${vaultData.marketType === 'lend' ? `Curve Lend on ${vaultData.network}` : 'crvUSD market'}\\)${typeof vaultData.externalUrl !== 'undefined' ? ` \\([link](${vaultData.externalUrl})\\)` : ''}:\n     ${textLines.join("\n     ")}`;
